32491e4528403ac8df8eb450c27bc8b90b009351
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-05 19:50:20 +0330

    client init

--- client.rb
+++ client.rb
@@ -1,1 +1,20 @@
-# Client
+# frozen_string_literal: true
+# Client browser
+
+require 'socket'
+print 'Domain >> '
+host = gets
+print 'Port   >> '
+port = gets
+print 'Path   >> '
+path = gets
+
+request = "GET #{path} HTTP/1.0\r\n\r\n"
+
+socket = TCPSocket.open(host.strip, port.to_i)
+socket.print(request)
+
+response = socket.read
+
+header, body = response.split("\r\n\r\n", 2)
+print header
