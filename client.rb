03884779c8cf30a7f3b02fcce920572d5aa40d58
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-05 19:56:38 +0330

    client google test successfully

--- client.rb
+++ client.rb
@@ -14,7 +14,13 @@ request = "GET #{path} HTTP/1.0\r\n\r\n"
 socket = TCPSocket.open(host.strip, port.to_i)
 socket.print(request)
 
+Dir.mkdir('response') unless File.exist?('response')
+
 response = socket.read
 
 header, body = response.split("\r\n\r\n", 2)
 print header
+
+File.open('./response/index.html', 'w') do |f|
+  f.write(body)
+end
